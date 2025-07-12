aa11860344c0f220f4280137cc0927cad78933cf
Dav1d-Rodr1guez <137791252+Dav1d-Rodr1guez@users.noreply.github.com>
2025-07-12 17:40:08 -0500

    add files update jul 2025 v2.0

--- js/main.js
+++ js/main.js
@@ -13,16 +13,30 @@
         return { cajas: 250, pallets: 6412 };
       case "arroz":
         return { cajas: 2000, pallets: 51300 };
-      case "bambu":
+      case "avena":
+        return { cajas: 2200, pallets: 56430 };
+      case "caña":
         return { cajas: 8400, pallets: 215460 };
       case "soja":
         return { cajas: 24000, pallets: 615600 };
+      case "tomate":
+        return { cajas: 26400, pallets: 677160 };
       case "col":
         return { cajas: 57000, pallets: 1477440 };
       case "zanahoria":
         return { cajas: 85000, pallets: 2180250 };
       case "papa":
         return { cajas: 120000, pallets: 3078000 };
+      case "pino":
+        return { cajas: 600, pallets: 15390 };
+      case "cacao":
+        return { cajas: 70000, pallets: 1795500 };
+      case "cipres":
+        return { cajas: 150000, pallets: 3847500 };
+      case "abedul":
+        return { cajas: 210000, pallets: 5386500 };
+      case "comida":
+        return { cajas: 450, pallets: 11542 };
       default:
         return { cajas: "", pallets: "" };
     }
@@ -42,22 +56,35 @@
       trigo: 2,
       maiz: 5,
       arroz: 40,
-      bambu: 168,
+      avena: 44,
+      caña: 168,
       soja: 480,
+      tomate: 528,
       col: 1140,
       zanahoria: 1700,
-      papa: 2400
+      papa: 2400,
+      pino: 600,
+      cacao:70000,
+      cipres: 150000,
+      abedul: 210000,
+      comida: 450,
     };
   
     const palletsPorProducto = {
       trigo: 2565 / 27 / 50, // Convertir de pallets a cajas
       maiz: 6412 / 27 / 50,
       arroz: 51300 / 27 / 50,
-      bambu: 215460 / 27 / 50,
+      avena: 56430 / 27 / 50,
+      caña: 215460 / 27 / 50,
       soja: 615600 / 27 / 50,
+      tomate: 677160 / 27 / 50,
       col: 1477440 / 27 / 50,
       zanahoria: 2180250 / 27 / 50,
-      papa: 3078000 / 27 / 50
+      papa: 3078000 / 27 / 50,
+      pino: 15390 / 27 / 50,
+      cacao: 1795500 / 27 / 50,
+      cipres: 3847500 / 27 / 50,
+      abedul: 5386500 / 27 / 50,
     };
   
     // Calcular el total de la venta
